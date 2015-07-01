2691c7dfdc13e771a1e04dbda99ae8364344868c
mfunaki <mfunaki@gmail.com>
2015-07-01 14:34:15 +0900

    Cosmetic modification.

--- src/net/mayoct/hcp/littlebits/CloudbitSubscriptionServlet.java
+++ src/net/mayoct/hcp/littlebits/CloudbitSubscriptionServlet.java
@@ -16,7 +16,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.sql.DataSource;
 
-import net.mayoct.hcp.littlebits.persistence.Cloudbit;
 import net.mayoct.hcp.littlebits.persistence.CloudbitEvent;
 import net.mayoct.hcp.littlebits.persistence.CloudbitEventDAO;
 
@@ -175,5 +174,4 @@ public class CloudbitSubscriptionServlet extends HttpServlet {
 		}
 		cloudbitEventDAO.addRow(eventData);
 	}
-	
 }
